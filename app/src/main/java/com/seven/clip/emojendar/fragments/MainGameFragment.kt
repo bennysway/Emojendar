80259cfb889fbdd95257bad6ef294ccc54a5131f
Ben <bcharumbira@lbpro.pl>
2020-01-10 08:23:29 +0100

    CHanged swipe view

--- app/src/main/java/com/seven/clip/emojendar/fragments/MainGameFragment.kt
+++ app/src/main/java/com/seven/clip/emojendar/fragments/MainGameFragment.kt
@@ -6,14 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.seven.clip.emojendar.R
-import com.seven.clip.emojendar.SimpleAdapter
-import kotlinx.android.synthetic.main.fragment_main_game.*
-import life.sabujak.shyshark.ShySharkLayoutManager.Companion.SWIPE_BOTTOM
-import life.sabujak.shyshark.ShySharkLayoutManager.Companion.SWIPE_LEFT
-import life.sabujak.shyshark.ShySharkLayoutManager.Companion.SWIPE_RIGHT
-import life.sabujak.shyshark.ShySharkLayoutManager.Companion.SWIPE_TOP
-import life.sabujak.shyshark.listener.OnSwipeListener
-
 
 class MainGameFragment :Fragment(){
     override fun onCreateView(
@@ -26,56 +18,6 @@ class MainGameFragment :Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        recyclerView.adapter = SimpleAdapter(
-            arrayListOf(
-                R.drawable.ic_1f478_1f3fc,
-                R.drawable.ic_1f3f4_2620,
-                R.drawable.ic_1f602,
-                R.drawable.ic_1f923,
-                R.drawable.ic_1f603,
-                R.drawable.ic_1f604,
-                R.drawable.ic_1f606,
-                R.drawable.ic_1f60a,
-                R.drawable.ic_1f60b
-            )
-        )
-        recyclerView.setOnSwipeListener(object :
-            OnSwipeListener {
-            override fun onChangeHorizontalDrag(direction: Int, percent: Float) {
-                when (direction) {
-                    SWIPE_LEFT -> {
-                    }
-                    SWIPE_RIGHT -> {
-                    }
 
-                }
-            }
-
-            override fun onChangeVerticalDrag(direction: Int, percent: Float) {
-                when (direction) {
-                    SWIPE_TOP -> {
-                    }
-                    SWIPE_BOTTOM -> {
-                    }
-                }
-            }
-
-            override fun onSwiped(position: Int, direction: Int) {
-                when (direction) {
-                    SWIPE_LEFT -> {
-                        println("swiped LEFT!! $position")
-                    }
-                    SWIPE_RIGHT -> {
-                        println("swiped RIGHT $position")
-                    }
-                    SWIPE_TOP -> {
-                        recyclerView.previousView()
-                    }
-                    SWIPE_BOTTOM -> {
-                        println("swiped BOTTOM $position")
-                    }
-                }
-            }
-        })
     }
 }
